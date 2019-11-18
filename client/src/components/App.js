1af47e43a8eb69a5d2a459f124a033a26547c701
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2019-11-18 20:21:24 +0700

    Reworked Webpack and Babel Issues

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import React, {Component, Fragment} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (<Fragment><h1>Hello how are you</h1></Fragment>)
+    }
+}
+
+ReactDOM.render (<App/>, document.getElementById("app"));
